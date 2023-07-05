dbe72a346bffea5527876df970497e6bcf338d8f
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-05 15:05:57 +0700

    primitive data types in typescript : string, number and boolean

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data type', () => {
+    it('should must declare', () => {
+        const name: string = 'Ahmad Lutfi Rizki Patria';
+        const balance: number = 1000000;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
