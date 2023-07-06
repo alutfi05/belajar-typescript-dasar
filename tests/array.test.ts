0be50f1eedd4d3fb58a3d094c12ffae54047c5c7
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-06 13:51:05 +0700

    learning data type array

---
+++ tests/array.test.ts
@@ -0,0 +1,9 @@
+describe('Array', () => {
+    it('should same with javascript', () => {
+        const names: string[] = ['Ahmad', 'Lutfi', 'Laila'];
+        const values: number[] = [1, 2, 3];
+
+        console.info(names);
+        console.info(values);
+    });
+});
