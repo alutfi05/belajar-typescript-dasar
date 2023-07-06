6acb93c032d45c14a86b496177f581977cae6eca
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-06 14:17:49 +0700

    checking data type using typeof in union type for secure data type in union type

--- tests/union.test.ts
+++ tests/union.test.ts
@@ -11,4 +11,20 @@ describe('Union Type', () => {
 
         // sample = []; // error
     });
+
+    it('should support typeof operator', () => {
+        const process = (value: number | string | boolean) => {
+            if (typeof value === 'string') {
+                return value.toUpperCase();
+            } else if (typeof value === 'number') {
+                return value + 2;
+            } else {
+                return !value;
+            }
+        };
+
+        expect(process('Lutfi')).toBe('LUTFI');
+        expect(process(100)).toBe(102);
+        expect(process(true)).toBe(false);
+    });
 });
